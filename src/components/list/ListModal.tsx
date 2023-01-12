618ac01379387bb3bc24e06672d9ab84e6cf2a00
Caesar <vuhoang7398@gmail.com>
2023-01-12 14:54:50 +0200

    Style CSS: addjust overflow for the modal

--- src/components/list/ListModal.tsx
+++ src/components/list/ListModal.tsx
@@ -32,6 +32,7 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    overflow: 'auto'
 };
 
 // ==============================|| MODAL - FOR LIST   ||============================== //
